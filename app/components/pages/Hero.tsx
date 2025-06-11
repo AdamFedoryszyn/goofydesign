1a85123bf4792c5d279d1f56e24cb8d8b48816d2
Adam Fedoryszyn <adamfedoryszyn@gmail.com>
2025-06-11 22:28:01 +0200

    Created text on Hero page and buttons, added images aswell

--- app/components/pages/Hero.tsx
+++ app/components/pages/Hero.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from 'next/image'
+import cat from '@/public/cat.png'
 
 export default function Hero() {
   return (
@@ -6,7 +8,7 @@ export default function Hero() {
       <h1 className='p-12 pl-32 font-bold text-2xl text-amber-200'>
         Goofy
       </h1>
-      <div className='border border-gray-50 border-b-0 border-r-0 border-t-0 h-screen w-screen flex justify-between'>
+      <div className='border-2 border-white/30 border-b-0 border-r-0 border-t-0 h-screen w-screen flex justify-between'>
         <div>
           <nav className='absolute'>
             <ul>
@@ -22,16 +24,25 @@ export default function Hero() {
             </ul>
           </nav>
         </div>
-        <div className= 'bg-green-400 w-8/10 h-full rounded-xl mt-8 mr-40 flex justify-between'>
+        <div className= 'bg-gradient-to-t from-green-500 to-green-300 w-8/11 h-full rounded-xl mt-8 mr-40 flex justify-between'>
           <div className='w-7/10 h-full bg-white mt-6 -ml-6 rounded-xl'>
+            <Image src={cat} alt='volcano' width={0} height={0} sizes="100vw" style={{ width: '100%', height: '400px' }} className='rounded-t-xl fit'></Image>
           </div>
           <div className='flex flex-col items-end ml-6 mr-4 mt-4 space-y-4'>
-            <div className='bg-gradient-to-tr from-yellow-500 to-orange-600 w-80 h-40 rounded-lg'></div>
-            <div className='bg-white rounded-lg h-88 w-95 mr-4'></div>
+            <div className='bg-gradient-to-br from-red-400 to-orange-300 w-80 h-44 rounded-lg flex'>
+              <div className='absolute h-20 w-80 p-2 text-white text-center'>
+                <p>Join now to access customizable <span className="font-bold">templates</span>, create stunning <span className="font-bold">designs</span>, and share your work with a creative <span className="font-bold">community</span></p>
+              </div>
+              <button className='bg-blue-500 h-12 w-48 rounded-3xl text-white ml-28 mt-28 relative'>
+                <span className="absolute top-0 right-0 p-2 pr-4">Sign up</span>
+              </button>
+            </div>
+            <div className='bg-white rounded-lg h-88 w-95 mr-4'>
+              <h1 className='text-black text-xl font-bold p-2 pl-4'>Check these out</h1>
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   )
 }
